1801bce98533ec4bc4b5c2f4a91bab82c0727c32
Medovyha <maxim.donchyk@gmail.com>
2023-07-09 14:09:14 +0200

    Initial commit

---
+++ src/main/java/SysLogApi/SysLogApi/SysLogApiApplication.java
@@ -0,0 +1,13 @@
+package SysLogApi.SysLogApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SysLogApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SysLogApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/SysLogApi/SysLogApi/SysLogApiApplicationTests.java
@@ -0,0 +1,13 @@
+package SysLogApi.SysLogApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SysLogApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
